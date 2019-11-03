ff7a15ba88f954946dd2b5255779844ed094e088
Ero Karlsen <erokar@gmail.com>
2019-11-03 16:45:02 +0100

    Add notes model and create superuser.

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
