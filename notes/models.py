77577a2dd27ecee125acd5fa7599d0ad5c13db07
Ero Karlsen <erokar@gmail.com>
2020-10-11 14:03:35 +0200

    Update models.py

--- notes/models.py
+++ notes/models.py
@@ -6,6 +6,8 @@ class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
